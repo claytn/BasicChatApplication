0b660b5ba7dd91b558babfdcfae8838d4d353380
marsha88 <marsha88@purdue.edu>
2017-01-16 16:13:10 -0500

    Added console messaging

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,14 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('connected to socket.io');
 });
+
+socket.on('pizza', function(data){
+	console.log(data.text);
+	//socket.emit('pizza', {text: "You've got mail."});
+});
+
+function sendMessage(){
+	var message = document.getElementById('message').value;
+	document.getElementById('message').value = "";
+	socket.emit('pizza', {text:message});
+}
--- server.js
+++ server.js
@@ -6,9 +6,14 @@ var io = require('socket.io')(http);
 
 app.use(express.static('./public'));
 
-io.on('connection', function(){
-	console.log('user connected');
+io.on('connection', function(socket){
+	socket.on('pizza', function(data){
+		socket.broadcast.emit('pizza', data);	
+	});
+
+	socket.emit('pizza', {date: new Date(), from: 'Server', text: 'Hello, welcome to the chat app.'});
 });
+
 
 http.listen(PORT, function(){
 	console.log('Server Started...');
