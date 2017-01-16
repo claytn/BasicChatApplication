d90db356385e555dbb395b3dfbb67de482085d8b
marsha88 <marsha88@purdue.edu>
2017-01-16 13:25:58 -0500

    Initialized front-end for use of sockets

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var PORT = process.env.PORT || 3000;
-
-const express = require('express');
-console.log(express.toString());
-
-var app = express();	//creates server
-var http = require('http').Server(app);	//
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('./public'));
 
-io.on('connection', function(socket){
-	//console.log('Got connected for free. At education connection.');
-	socket.on('message', function(message){
-		console.log('Message Received:', message.text);
-		io.emit('message', message);
-	});
-
-	socket.emit('message', {
-		text: 'Welcome to the Chat Application'
-	});
+io.on('connection', function(){
+	console.log('user connected');
 });
 
 http.listen(PORT, function(){
-	console.log('Server running on', PORT);
+	console.log('Server Started...');
 });
