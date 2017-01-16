2fb51a46b6007f0c350b3dcd8778efccb7401fbe
marsha88 <marsha88@purdue.edu>
2017-01-16 15:41:41 -0500

    updated front-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 var socket = io();
-socket.on('connect', function(){
-	console.log('Connected to socket.io');
-});
 
-socket.on('message', function(message){
-	console.log(message.text);
+socket.on('connect', function(){
+	console.log('connected to socket.io');
 });
